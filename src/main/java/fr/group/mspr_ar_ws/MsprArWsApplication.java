e9dc7fc03bd9d49bd99f4895f1afeb261632ac4c
Hichem <hichem.hammadache@gmail.com>
2023-01-06 13:39:58 +0100

    second commit

---
+++ src/main/java/fr/group/mspr_ar_ws/MsprArWsApplication.java
@@ -0,0 +1,13 @@
+package fr.group.mspr_ar_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsprArWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsprArWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/group/mspr_ar_ws/MsprArWsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.group.mspr_ar_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsprArWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
